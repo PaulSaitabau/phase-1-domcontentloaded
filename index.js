ab3eaaa70604fad5a240caa22c28cba8c65956a1
paulsaitabau <paulsaitabau05@gmail.com>
2024-04-07 23:05:44 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+function updateDOM() {
+    // Find the HTML element with the ID "text"
+    var textElement = document.getElementById("text");
+
+    // Update the content of the element
+    textElement.innerHTML = "This is really cool!";
+}
+
+// calls the updateDOM function
+document.addEventListener("DOMContentLoaded", function() {
+    updateDOM();
+});
